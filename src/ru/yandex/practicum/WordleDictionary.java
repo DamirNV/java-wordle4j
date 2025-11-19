af308bbe2d4f441c86d8be44be07840db7c313b3
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-19 09:10:01 +0300

    feat: написать базовую функциональность для словаря

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -10,5 +12,23 @@ import java.util.List;
 public class WordleDictionary {
 
     private List<String> words;
+    private Random random;
+
+    public WordleDictionary() {
+        this.words = new ArrayList<>();
+        this.words.add("стол");
+        this.words.add("стул");
+        this.words.add("ручка");
+        this.words.add("бумага");
+        this.random = new Random();
+    }
+
+    public boolean contains(String word) {
+        return words.contains(word.toLowerCase());
+    }
+
+    public String getRandomWord() {
+        return words.get(random.nextInt(words.size()));
+    }
 
 }
--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,54 @@
 package ru.yandex.practicum;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+
+
+    public WordleDictionary loadDictionary(String filename) {
+        List<String> words = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8))) {
+
+            String line;
+            int totalWords = 0;
+            int fiveLetterWords = 0;
+
+            while ((line = reader.readLine()) != null) {
+                totalWords++;
+                String formattedWord = formatWord(line.trim());
+                if (formattedWord.length() == 5) {
+                    words.add(formattedWord);
+                    fiveLetterWords++;
+                }
+            }
+
+
+
+        } catch (IOException e) {
+
+        }
+
+        return new WordleDictionary(words);
+    }
+
+    private String formatWord(String word) {
+        return word.toLowerCase()
+                .replace('ё', 'е')
+                .trim();
+    }
+
 }
