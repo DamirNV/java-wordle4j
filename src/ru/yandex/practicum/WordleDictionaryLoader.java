31768d5a27aabd0b9ca8decce2abd3fb0eacd243
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-19 14:51:41 +0300

    feat: добавить отлов исключений в WordleDictionaryLoader

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum;
 
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,35 +8,25 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
-    ему нужны методы по загрузке списка слов из файла по имени файла
-    на выходе должен быть класс WordleDictionary
- */
 public class WordleDictionaryLoader {
 
     public WordleDictionary loadDictionary(String filename) {
-
         List<String> words = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8))) {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename),
+                StandardCharsets.UTF_8))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
-
                 String formattedWord = formatWord(line.trim());
                 if (formattedWord.length() == 5) {
                     words.add(formattedWord);
-
                 }
             }
-
+            if (words.isEmpty()) {
+                throw new WordleGameException("Словарь пуст или не содержит 5-буквенных слов");
+            }
         } catch (IOException e) {
-
+            throw new WordleGameException("Ошибка загрузки файла: " + e.getMessage());
         }
-
         return new WordleDictionary(words);
     }
 
