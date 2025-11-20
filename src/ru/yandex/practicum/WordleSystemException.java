5d2acaa152ff6c4deac33dd26d75c63ca84fb111
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-20 15:52:24 +0300

    feat: добавить конструктор в исключение WordleSystemException

--- src/ru/yandex/practicum/WordleSystemException.java
+++ src/ru/yandex/practicum/WordleSystemException.java
@@ -4,4 +4,8 @@ public class WordleSystemException extends RuntimeException {
     public WordleSystemException(String message) {
         super(message);
     }
+
+    public WordleSystemException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
